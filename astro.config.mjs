f77adcd94f4255ab3e601e6d740cb34eda0b0326
Leandro Gomez <leandro.grotnig@gmail.com>
2025-04-24 14:43:36 -0300

    Adds tailwind and base deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+import cloudflare from "@astrojs/cloudflare";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: "https://example.com",
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+    },
+    adapter: cloudflare(),
+    integrations: [mdx(), sitemap()],
 });
